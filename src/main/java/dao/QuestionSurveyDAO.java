76023cfde59bb058d77666af835da345370d08b2
fdc-linchien <lin_linchien@fannel.com>
2026-06-02 15:57:37 +0900

    飼い主用とペット用のアンケート機能

---
+++ src/main/java/dao/QuestionSurveyDAO.java
@@ -0,0 +1,48 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Question;
+
+public class QuestionSurveyDAO {
+	
+	private final String JDBC_URL =
+			"jdbc:sqlserver://localhost\\\\\\\\SQLEXPRESS:53375;databaseName=master;integratedSecurity=true;encrypt=true;trustServerCertificate=true";
+	public List<Question> findAllQuestion(){
+		List<Question> questionSurveyList = new ArrayList<>();
+	
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		}catch(ClassNotFoundException e) {
+			throw new IllegalStateException("JDBCドライバを読み込めませんでした");
+		}
+		try (Connection conn = DriverManager.getConnection(JDBC_URL)){
+			
+			String sql = "SELECT * FROM QuestionSurvey";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+			
+			ResultSet rs = pStmt.executeQuery();
+			
+			while(rs.next()) {
+				int QuestionID = rs.getInt("QuestionID");
+				String userQuestion = rs.getString("userQuestion");
+				String petQuestion = rs.getString("petQuestion");
+				Question question = new Question(QuestionID,userQuestion,petQuestion);
+				questionSurveyList.add(question);
+			}
+			
+		
+		}catch(SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return questionSurveyList;
+	}
+
+}
---
+++ src/main/java/dao/SurveyChoiceDAO.java
@@ -0,0 +1,50 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Choice;
+
+public class SurveyChoiceDAO {
+
+	
+	private final String JDBC_URL =
+			"jdbc:sqlserver://localhost\\\\\\\\SQLEXPRESS:53375;databaseName=master;integratedSecurity=true;encrypt=true;trustServerCertificate=true";
+	public List<Choice> findAllChoices(){
+		List<Choice> allChoiceList = new ArrayList<>();
+	
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		}catch(ClassNotFoundException e) {
+			throw new IllegalStateException("JDBCドライバを読み込めませんでした");
+		}
+		try (Connection conn = DriverManager.getConnection(JDBC_URL)){
+			
+			String sql = "SELECT * FROM SurveyChoice";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+			
+			ResultSet rs = pStmt.executeQuery();
+			
+			while(rs.next()) {
+				int SurveyChoiceID = rs.getInt("SurveyChoiceID");
+				int QuestionID = rs.getInt("QuestionID");
+				String Choice = rs.getString("Choice");
+				Choice choice = new Choice(SurveyChoiceID,QuestionID,Choice);
+				allChoiceList.add(choice);
+			}
+			
+		
+		}catch(SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+		return allChoiceList;
+	}
+
+	
+}
