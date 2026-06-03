76d7dee75ad8a8351e027907662340ca1452c81b
fsz-moriuchi <daiki_moriuchi@fsz.co.jp>
2026-06-03 16:45:13 +0900

    [DAO]変更

--- src/main/java/dao/PetListDAO.java
+++ src/main/java/dao/PetListDAO.java
@@ -8,16 +8,78 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import model.Pet;
 import model.PetDetail;
+import model.PetInformation;
 import model.PetInformationView;
-
 
 public class PetListDAO {
 	private final String JDBC_URL = "jdbc:sqlserver://localhost\\\\SQLEXPRESS:58956;databaseName=master;integratedSecurity=true;encrypt=true;trustServerCertificate=true";
 
+	public boolean createPet(Pet pet) {
+
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		} catch (ClassNotFoundException e) {
+			throw new IllegalStateException("JDBCドライバを読み込めませんでした");
+		}
+		try (Connection conn = DriverManager.getConnection(JDBC_URL)) {
+
+			String sql = "INSERT INTO Pet(petID,category) VALUES(?, ?)";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			pStmt.setInt(1, pet.getPetID());
+			pStmt.setString(2, pet.getCategory());
+
+			int result = pStmt.executeUpdate();
+			if (result != 1) {
+				return false;
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
+	public boolean createPetInformation(PetInformation petInformation) {
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		} catch (ClassNotFoundException e) {
+			throw new IllegalStateException("JDBCドライバを読み込めませんでした");
+		}
+		try (Connection conn = DriverManager.getConnection(JDBC_URL)) {
+
+			String sql = "INSERT INTO PetInformation( petInformationID, petID, name, gender, age, color, pet_size, vaccine, price, commentText) VALUES(?,?,?,?,?,?,?,?,?,?)";
+			PreparedStatement pStmt = conn.prepareStatement(sql);
+
+			pStmt.setInt(1, petInformation.getPetInformationID());
+			pStmt.setInt(2, petInformation.getPetID());
+			pStmt.setString(3, petInformation.getName());
+			pStmt.setString(4, petInformation.getGender());
+			pStmt.setInt(5, petInformation.getAge());
+			pStmt.setString(6, petInformation.getColor());
+			pStmt.setString(7, petInformation.getPet_size());
+			pStmt.setString(8, petInformation.getVaccine());
+			pStmt.setInt(9, petInformation.getPrice());
+			pStmt.setString(10, petInformation.getCommentText());
+
+			int result = pStmt.executeUpdate();
+			if (result != 1) {
+				return false;
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+
 	public List<PetInformationView> showList() {
 		List<PetInformationView> petList = new ArrayList<>();
-		
+
 		try {
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 		} catch (ClassNotFoundException e) {
@@ -48,8 +110,9 @@ public class PetListDAO {
 		return petList;
 
 	}
+
 	public PetDetail showPetDetail(int petID) {
-		PetDetail petDetail = null; 
+		PetDetail petDetail = null;
 		try {
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 		} catch (ClassNotFoundException e) {
@@ -62,7 +125,7 @@ public class PetListDAO {
 			String sql = "SELECT P.petID, P.category, PI.petInformationID, PI.name, PI.gender, PI.age, PI.color, PI.pet_size, PI.vaccine, PI.price, PI.commentText FROM Pet P JOIN PetInformation PI ON P.petID = PI.petID WHERE P.petID = ?";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 			pStmt.setInt(1, petID);
-			
+
 			ResultSet rs = pStmt.executeQuery();
 
 			while (rs.next()) {
@@ -76,7 +139,8 @@ public class PetListDAO {
 				String vaccine = rs.getString("vaccine");
 				int price = rs.getInt("price");
 				String commentText = rs.getString("commentText");
-				petDetail = new PetDetail(petID, category, petInformationID, name, gender, age, color, pet_size, vaccine, price, commentText);
+				petDetail = new PetDetail(petID, category, petInformationID, name, gender, age, color, pet_size,
+						vaccine, price, commentText);
 			}
 		} catch (SQLException e) {
 			e.printStackTrace();
