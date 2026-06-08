6bd372f915184da2ad351e7ec50f8b2fef4edd20
fsz-funamori <asato_funamori@fsz.co.jp>
2026-06-08 12:58:15 +0900

    ブランチ切り替えエラー

--- src/main/java/dao/UsersDAO.java
+++ src/main/java/dao/UsersDAO.java
@@ -1,18 +1,15 @@
 package dao;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 
-import model.UserLogin;
 import model.User;
+import model.UserLogin;
+import util.DButil;
 
 public class UsersDAO {
-	private final String JDBC_URL = "jdbc:sqlserver://localhost\\\\\\\\SQLEXPRESS:61371;databaseName=master;integratedSecurity=true;encrypt=true;trustServerCertificate=true;";
-
-	public User findByLogin(UserLogin login) {
+	public User findByLogin(UserLogin login){
 		User user = null;
 
 		try {
@@ -21,7 +18,7 @@ public class UsersDAO {
 			throw new IllegalStateException("JDBCドライバを読み込めませんでした");
 		}
 
-		try (Connection conn = DriverManager.getConnection(JDBC_URL)) {
+		try (Connection conn = DButil.getConnection()) {
 
 			String sql = "SELECT USER_ID, PASSWORD_HASH FROM USERS WHERE USER_ID = ? AND PASSWORD_HASH= ?";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
@@ -35,20 +32,20 @@ public class UsersDAO {
 				String passwordHash = rs.getString("PASSWORD_HASH");
 				user = new User(userId, passwordHash);
 			}
-		} catch (SQLException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			return null;
 		}
 		return user;
 	}
-	
-	public boolean registerUser(User user) {
+
+	public boolean registerUser(User user){
 		try {
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 		} catch (ClassNotFoundException e) {
 			throw new IllegalStateException("JBDCドライバを読み込めませんでした");
 		}
-		try (Connection conn = DriverManager.getConnection(JDBC_URL)) {
+		try (Connection conn = DButil.getConnection()) {
 
 			String sql = "INSERT INTO USERS(USER_ID, PASSWORD_HASH) VALUES(?, ?)";
 			PreparedStatement pStmt = conn.prepareStatement(sql);
@@ -60,12 +57,11 @@ public class UsersDAO {
 			if (result != 1) {
 				return false;
 			}
-		} catch (SQLException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 			return false;
 		}
 		return true;
 	}
-
 
 }
